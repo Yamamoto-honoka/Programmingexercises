f2baf662a81c85c13513ef0e3569fc08925be20b
Yamamoto-honoka <kirakirahonoka@gmail.com>
2025-05-13 22:36:36 +0900

    5/13 ブラケットで作成開始。

---
+++ Registration_sample/regist.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>diworksblog 掲示板</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+    <div class="logo"><img src="diblog_logo.jpg"></div>
+    <header>
+        <ul>
+            <li>トップ</li>
+            <li>プロフィール</li>
+            <li>D.I.Blogについて</li>
+            <li>登録フォーム</li>
+            <li>問い合わせ</li>
+            <li>その他</li>
+            <li><a href = "http://localhost/Registration_sample/regist.php">アカウント登録</a></li>
+        </ul>
+    </header>
+    
+    <footer>
+        Copyright D.I.works D.I.blog is the one which provides A to Z about programming
+    </footer>
+    </body>
+</html>
