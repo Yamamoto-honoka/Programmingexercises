58ef5026f7607d6e5eb8a8f77c623480e8449dce
Yamamoto-honoka <kirakirahonoka@gmail.com>
2025-05-14 22:55:13 +0900

    5/14 登録画面作成スタート

--- Registration_sample/regist.php
+++ Registration_sample/regist.php
@@ -9,7 +9,7 @@
     <div class="logo"><img src="diblog_logo.jpg"></div>
     <header>
         <ul>
-            <li>トップ</li>
+            <li><a href="index.html">トップ</a></li>
             <li>プロフィール</li>
             <li>D.I.Blogについて</li>
             <li>登録フォーム</li>
@@ -18,7 +18,33 @@
             <li><a href = "http://localhost/Registration_sample/regist.php">アカウント登録</a></li>
         </ul>
     </header>
-    
+    <h1>アカウント登録画面</h1>
+    <form class="regist" method="post" action="regist_confirm.php">
+        <div>
+            <label>名前(姓)</label>
+            <input type="text" class="text" size="35"
+            maxlength="10" name="family_name"
+            pattern="[\u4E00-\u9FFF\u3040-\u309Fー]*">
+        </div>
+        <div>
+        <label>名前(名)</label>
+        <input type="text" class="text" size="35"
+        maxlength="10" name="last_name"
+        pattern="[\u4E00-\u9FFF\u3040-\u309Fー]*">
+        </div>
+        <div>
+        <label>カナ(姓)</label>
+        <input type="text" class="text" size="35"
+        maxlength="10" name="family_name_kana"
+        pattern="[\u30A1-\u30FF]*">
+        </div>
+        <div>
+        <label>カナ(名)</label>
+        <input type="text" class="text" size="35"
+        maxlength="10" name="last_name_kana"
+        pattern="[\u30A1-\u30FF]*">
+        </div>
+    </form>
     <footer>
         Copyright D.I.works D.I.blog is the one which provides A to Z about programming
     </footer>
