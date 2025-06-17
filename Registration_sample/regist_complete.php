83da49ac62d336a1b733d50f751a8d37db73d394
Yamamoto-honoka <kirakirahonoka@gmail.com>
2025-06-17 22:18:10 +0900

    6/17 完了画面が出来たけど… MySQLが重すぎてテーブル名が分からん…。後は諸々の詳設定のみ…かな。

---
+++ Registration_sample/regist_complete.php
@@ -0,0 +1,50 @@
+<?php
+mb_internal_encoding("utf8");
+$pdo = new PDO("mtsql:dbname = programingexe;host = localhost;",
+               "root","");
+$pdo -> exec("insert into (family_name, last_name, family_name_kana, last_name_kana, mail, password, gender, postal_code, prefecture, address_1, address_2, authority)
+values(
+'".$_POST['family_name']."',
+'".$_POST['last_name']."',
+'".$_POST['family_name_kana']."',
+'".$_POST['last_name_kana']."',
+'".$_POST['mail']."',
+'".$_POST['password']."',
+'".$_POST['gender']."',
+'".$_POST['postal_code']."',
+'".$_POST['prefecture']"',
+'".$_POST['address_1']."',
+'".$_POST['address_2']."',
+'"$_POST['authority']."');");
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>diworksblog 掲示板</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+    <div class="logo"><img src="diblog_logo.jpg"></div>
+    <header>
+        <ul>
+            <li><a href="index.html">トップ</a></li>
+            <li>プロフィール</li>
+            <li>D.I.Blogについて</li>
+            <li>登録フォーム</li>
+            <li>問い合わせ</li>
+            <li>その他</li>
+            <li><a href = "http://localhost/Registration_sample/regist.php">アカウント登録</a></li>
+        </ul>
+    </header>
+    <h1>アカウント登録完了画面</h1>
+        <div class="comfirm">
+        <p>登録完了しました。</p>
+        <button onclick="index.html">TOPページへ戻る</button>
+        </div>
+    <footer>
+        Copyright D.I.works D.I.blog is the one which provides A to Z about programming
+    </footer>
+    </body>
+</html>
