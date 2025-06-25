1e3de1523d602c916097f7c69b3d6cfe79ee6ef8
Yamamoto-honoka <kirakirahonoka@gmail.com>
2025-06-25 21:48:57 +0900

    6/25 最初からエラー…。見直してみます。

--- Registration_sample/regist_complete.php
+++ Registration_sample/regist_complete.php
@@ -2,8 +2,10 @@
 mb_internal_encoding("utf8");
 $pdo = new PDO("mysql:dbname=programingexe01;host=localhost;",
                "root","");
-$pdo ->exec("insert into userregistration(family_name, last_name, family_name_kana, last_name_kana, mail, password, gender, postal_code, prefecture, address_1, address_2, authority)
-values('".$_POST['family_name']."','".$_POST['last_name']."','".$_POST['family_name_kana']."','".$_POST['last_name_kana']."','".$_POST['mail']."','".$_POST['password']."','".$_POST['gender']."','".$_POST['postal_code']."','".$_POST['prefecture']."','".$_POST['address_1']."','".$_POST['address_2']."','".$_POST['authority']."');
+$pdo ->exec("insert into userregistration(
+family_name)
+values(
+'".$_POST['family_name']."');
 ");
 ?>
 
