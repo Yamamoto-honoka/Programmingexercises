32d08960a70f66afefc308982f37d0e49b3e457d
Yamamoto-honoka <kirakirahonoka@gmail.com>
2025-12-05 21:41:25 +0900

    12/5 エラー表示が…。

--- Registration_sample/regist.php
+++ Registration_sample/regist.php
@@ -153,6 +153,12 @@
         </div>
         <div id="submit">
             <input type="submit" value="確認する"/>
+            <?php
+            $error1="名前（姓）が未入力です。";
+            if(empty($_POST["family_name"])){
+                echo"$error1";
+            }
+            ?>
         </div>
     </form>
     <footer>
@@ -160,4 +166,4 @@
     </footer>
     </body>
 </html>
-<!--課題→エラーチェックとエラーメッセージ表示→JavaScriptで解決できるっぽい。-->
+<!--課題→エラーチェックとエラーメッセージ表示→PHPだけで解決できるっぽい？-->
